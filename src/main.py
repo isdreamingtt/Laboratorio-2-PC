4ce895f7a23b7bdd69c603ba3be8430158231598
Rona <harold.ramos@alumnos.ucn.cl>
2026-06-14 05:01:21 -0400

    Avance: Se crearon las clases para la limpieza de datos
    Se implemento la clase Preprocesador: Realiza todo el trabajo de limpieza para los versiculos
    -Transforma los versiculos en minusculas.
    -Elimina caracteres especiales, numeros y signos de puntuación.
    -Tokeniza el versiculo ya limpiado y elimina las StopWord(las cuales estan definidas para tener un mayor control)
    
    Se implemento la clase TFIDF:
    -TF: Que tan seguido aparece una palabra en un versiculo.
    -IDF: Que tan rara es la palabra en todo el corpus.
    
    Se implemento la clase Similitud Coseno: Calcula que tan parecidos son dos versiculos
    - 0: No hay ninguna similitud
    - 1: Son identicos.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from preprocesamiento import Preprocesador
+from tfidf import CalculadorTFIDF
 
 def main():
     print("Iniciando lectura del dataset...")
@@ -31,9 +32,7 @@ def main():
         ["id", "id_libro", "libro", "testamento", "id_genero", "capitulo", "versiculo", "texto"]
     ]
 
-
-    print("\nPrimeras filas del corpus:")
-    
+    print("\nPrimeras 5 filas del corpus base:")
     print(corpus.head())
 
     print("\nCantidad total de versículos:")
@@ -45,10 +44,62 @@ def main():
     print("\nCantidad por testamento:")
     print(corpus["testamento"].value_counts())
 
-    corpus.to_csv("datos/dataset_procesado/corpus_base.csv", index=False)
+    preprocesador = Preprocesador()
 
-    print("\nArchivo guardado en datos/dataset_procesado/corpus_base.csv")
+    textos_limpios = []
+    tokens_procesados = []
+    lista_tokens = []
 
+    for texto in corpus["texto"]:
+        texto_limpio, tokens = preprocesador.preprocesar_texto(texto)
+
+        textos_limpios.append(texto_limpio)
+        tokens_procesados.append(" ".join(tokens))
+        lista_tokens.append(tokens)
+
+    corpus["texto_limpio"] = textos_limpios
+    corpus["tokens"] = tokens_procesados
+
+    print("\nPrimeras 5 filas del corpus preprocesado:")
+    print(corpus.head())
+
+    vocabulario, frecuencias = preprocesador.construir_vocabulario_y_frecuencias(lista_tokens)
+
+    print("\nCantidad de palabras únicas en el vocabulario:")
+    print(len(vocabulario))
+
+    frecuencias_df = pd.DataFrame(list(frecuencias.items()), columns=["palabra", "frecuencia"])
+    frecuencias_df = frecuencias_df.sort_values("frecuencia", ascending=False).reset_index(drop=True)
+
+    print("\n20 palabras más frecuentes:")
+    print(frecuencias_df.head(20))
+
+    print("\nCalculando TF-IDF -> prueba para ver si funciona")
+
+    calculador_tfidf = CalculadorTFIDF()
+    
+    matriz_tfidf = calculador_tfidf.calcular_tfidf_corpus(lista_tokens)
+
+    print("\nCantidad de documentos vectorizados:", len(matriz_tfidf))
+    print("\nCantidad de palabras con IDF:", len(calculador_tfidf.idf))
+
+    print("\nTop 10 palabras TF-IDF del primer versículo:")
+    top_primer_versiculo = calculador_tfidf.obtener_top_tfidf(matriz_tfidf[0], 10)
+
+    for palabra, valor in top_primer_versiculo:
+        print(palabra, ":", valor)
+
+    #Calculo de tf-idf para el primer versículo, solo es una prueba para verificar que el proceso se realizó correctamente.
+    
+
+    corpus.to_csv("datos/dataset_procesado/corpus_preprocesado.csv", index=False)
+    frecuencias_df.to_csv("datos/dataset_procesado/frecuencias_palabras.csv", index=False)
+
+    print("\nArchivo guardado en:")
+    print("datos/dataset_procesado/corpus_preprocesado.csv")
+
+    print("\nFrecuencias guardadas en:")
+    print("datos/dataset_procesado/frecuencias_palabras.csv")
 
 if __name__ == "__main__":
     main()
